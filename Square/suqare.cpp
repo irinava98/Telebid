839458a5f2389289797ccdaaeec12a57f95f16fd
irinava98 <79048861+irinava98@users.noreply.github.com>
2022-02-01 20:45:32 +0200

    Update suqare.cpp

--- Square/suqare.cpp
+++ Square/suqare.cpp
@@ -5,13 +5,40 @@ using namespace std;
 
 int main()
 {
-    int A;
+
+
+ int A;
+ while(true)
+ {
     cout<<"A = ";
     cin>>A;
+    if(cin.fail())
+    {
+       cin.clear();
+       cin.ignore();
+       cout << "You entered not a number. Try again." << endl;
+    }
+    else if(A<=0 || A>=20000)
+    {
+
+       cin.clear();
+       cin.ignore();
+       cout << "You entered a number that is out of range. The range is (0,20000). Try again." << endl;
+    }
+    else
+    {
+        break;
+    }
+}
+
     int maxLength=0;
     set<int> distances;
-
-
+    /**
+    int longestX1=0;
+	int longestY1=0;
+	int longestX2=0;
+	int longestY2=0;
+	*/
     for(int x1=0;x1<=A;x1++)
     {
         for(int x2=0;x2<=A;x2++)
@@ -32,7 +59,12 @@ int main()
                           {
 
                               maxLength=distance;
-
+                              /**
+                              longestX1=x1;
+							  longestY1=y1;
+							  longestX2=x2;
+							  longestY2=y2;
+							  */
                           }
 
                         }
@@ -43,5 +75,6 @@ int main()
         }
     }
     int count=distances.size();
+    ///cout<<longestX1<<" "<<longestY1<<" "<<longestX2<<" "<<longestY2<<endl;
     cout<<maxLength<<"  "<<count<<endl;
 }
